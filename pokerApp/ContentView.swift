4bccaeb5de27f3b4428a4180c7a135b38d1c5dfa
keegan <letmeuseai66@gmail.com>
2024-10-21 13:41:15 -0700

    Initial Commit

---
+++ pokerApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pokerApp
+//
+//  Created by Norgard, Keegan - Student on 10/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pokerApp/pokerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokerAppApp.swift
+//  pokerApp
+//
+//  Created by Norgard, Keegan - Student on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct pokerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
