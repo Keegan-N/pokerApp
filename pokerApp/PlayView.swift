449a0a98fb1455744d5ec00632d3adf882fbe948
keegan <letmeuseai66@gmail.com>
2024-10-28 12:17:39 -0700

    not much logic no cards in middle yet

---
+++ pokerApp/PlayView.swift
@@ -0,0 +1,18 @@
+//
+//  PlayView.swift
+//  pokerApp
+//
+//  Created by Norgard, Keegan - Student on 10/25/24.
+//
+
+import SwiftUI
+
+struct PlayView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PlayView()
+}
